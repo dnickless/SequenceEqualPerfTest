6131427dc2bd4ceca2408459addcb21da4fa48bd
dnickless <daniel.hegener@gmx.net>
2017-08-21 01:20:25 -0400

    Add project files.

---
+++ SequenceEqualComparison/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SequenceEqualComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
