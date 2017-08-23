e766a41d1398ae7f9707a2394ba8ab79b96cd44d
dnickless <daniel.hegener@gmx.net>
2017-08-23 16:45:54 -0400

    Initial commit

---
+++ SequenceEqualComparison/EnumerableNew.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SequenceEqualComparison
+{
+    public static class EnumerableNew
+    {
+        public static bool SequenceEqualNew<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second) =>
+            SequenceEqualNew(first, second, null);
+
+        public static bool SequenceEqualNew<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        {
+            if (comparer == null)
+            {
+                comparer = EqualityComparer<TSource>.Default;
+            }
+
+            if (first == null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+
+            if (second == null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+
+            if (first is ICollection<TSource> firstCol && second is ICollection<TSource> secondCol)
+            {
+                if (firstCol.Count != secondCol.Count)
+                {
+                    return false;
+                }
+
+                if (firstCol is IList<TSource> firstList && secondCol is IList<TSource> secondList)
+                {
+                    int count = firstCol.Count;
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (!comparer.Equals(firstList[i], secondList[i]))
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+            }
+
+            using (IEnumerator<TSource> e1 = first.GetEnumerator())
+            using (IEnumerator<TSource> e2 = second.GetEnumerator())
+            {
+                while (e1.MoveNext())
+                {
+                    if (!(e2.MoveNext() && comparer.Equals(e1.Current, e2.Current)))
+                    {
+                        return false;
+                    }
+                }
+
+                return !e2.MoveNext();
+            }
+        }
+    }
+}
--- SequenceEqualComparison/Program.cs
+++ SequenceEqualComparison/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Jobs;
+using BenchmarkDotNet.Running;
 
 namespace SequenceEqualComparison
 {
@@ -6,7 +11,86 @@ namespace SequenceEqualComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<BenchmarkUsingIEnumerable>();
+            BenchmarkRunner.Run<BenchmarkUsingArray>();
+            BenchmarkRunner.Run<BenchmarkUsingList>();
+            BenchmarkRunner.Run<BenchmarkUsingImmutableList>();
+            BenchmarkRunner.Run<BenchmarkUsingSortedSet>();
+        }
+    }
+
+    [CoreJob]
+    public abstract class BenchmarkBase
+    {
+        protected IEnumerable<int> l1;
+        protected IEnumerable<int> l2;
+
+        [Params(100, 10000, 1000000, int.MaxValue)]
+        public int Count;
+
+        [GlobalSetup]
+        public void Initialize()
+        {
+            Initialize(Enumerable.Range(1, Count));
+        }
+
+        protected abstract void Initialize(IEnumerable<int> range);
+
+        [Benchmark(Description = "New")]
+        public void TestArrayNew()
+        {
+            bool b = l1.SequenceEqualNew(l2);
+        }
+
+        [Benchmark(Description = "Old")]
+        public void TestArrayOld()
+        {
+            bool b = l1.SequenceEqual(l2);
+        }
+    }
+
+    public class BenchmarkUsingArray : BenchmarkBase
+    {
+        protected override void Initialize(IEnumerable<int> range)
+        {
+            l1 = range.ToArray();
+            l2 = range.ToArray();
+        }
+    }
+
+    public class BenchmarkUsingList : BenchmarkBase
+    {
+        protected override void Initialize(IEnumerable<int> range)
+        {
+            l1 = range.ToList();
+            l2 = range.ToList();
+        }
+    }
+
+    public class BenchmarkUsingImmutableList : BenchmarkBase
+    {
+        protected override void Initialize(IEnumerable<int> range)
+        {
+            l1 = ImmutableList.Create(range.ToArray());
+            l2 = ImmutableList.Create(range.ToArray());
+        }
+    }
+
+    public class BenchmarkUsingSortedSet : BenchmarkBase
+    {
+        protected override void Initialize(IEnumerable<int> range)
+        {
+            l1 = new SortedSet<int>(range);
+            l2 = new SortedSet<int>(range);
+        }
+    }
+
+    public class BenchmarkUsingIEnumerable : BenchmarkBase
+    {
+        protected override void Initialize(IEnumerable<int> range)
+        {
+            l1 = range;
+            l2 = range;
         }
     }
 }
